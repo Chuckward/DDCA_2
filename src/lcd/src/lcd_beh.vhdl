1802723514df288002fad37961597203c018a0ae
Chuckward <andreas_ciachi@hotmail.com>
2016-11-03 18:56:17 +0100

    working on FSM;

--- src/lcd/src/lcd_beh.vhdl
+++ src/lcd/src/lcd_beh.vhdl
@@ -9,36 +9,97 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use work.lcd_pkg.all;
 
 architecture beh of lcd_controller is
 	type LCD_STATE_TYPE is (
 		STATE_INIT,
 		STATE_IDLE,
-		STATE_WAIT_START_BIT,
-		STATE_MIDDLE_OF_START_BIT,
-		STATE_GOTO_MIDDLE_OF_START_BIT,
-		STATE_WAIT_DATA_BIT,
-		STATE_MIDDLE_OF_DATA_BIT,
-		STATE_WAIT_STOP_BIT,
-		STATE_MIDDLE_OF_STOP_BIT
+		STATE_SET_CHAR,
+		STATE_DELETE,
+		STATE_CLR_SCREEN,
+		STATE_MOVE_CURSOR_NEXT,
+		STATE_NEW_LINE,
+		STATE_CFG,
+		STATE_SET_CURSOR_POS
 	);
 	
 	signal lcd_state, lcd_state_next	:	LCD_STATE_TYPE;
 	signal clk_cnt, clk_cnt_next		:	integer range 0 to 1;
+	signal lcd_busy						:	boolean;
 	
 begin
 
-	next_state : process(clk_cnt)
+	next_state : process(clk_cnt, instr)
 	begin
-		-- TODO: depending on output behavior, most of the states come from
-		-- the input expected from the output controller (instr and instr_data)
+		lcd_state <= lcd_state_next;
+		lcd_busy <= true;
+		
+		case lcd_state is
+			when STATE_INIT =>
+				null;
+			-- TODO: depending on output behavior, most of the states come from
+			when STATE_IDLE =>
+				if instr(1 downto 0) = INSTR_SET_CHAR then
+					lcd_state_next <= STATE_SET_CHAR;
+				elsif instr = INSTR_CLR_SCREEN then
+					lcd_state_next <= STATE_CLR_SCREEN;
+				elsif instr = INSTR_SET_CURSOR_POS then
+					lcd_state_next <= STATE_SET_CURSOR_POS;
+				elsif instr = INSTR_CONFIG then
+					lcd_state_next <= STATE_CFG;
+				elsif instr = INSTR_DELETE then
+					lcd_state_next <= STATE_DELETE;
+				elsif instr = INSTR_MOVE_CURSOR then
+					lcd_state_next <= STATE_MOVE_CURSOR_NEXT;
+				elsif instr = INSTR_NEW_LINE then
+					lcd_state_next <= STATE_NEW_LINE;
+				else 
+					lcd_state_next <= STATE_IDLE;
+					lcd_state <= false;
+				end if;
+			when STATE_SET_CHAR =>
+			
+			when STATE_DELETE =>
+			
+			when STATE_CLR_SCREEN =>
+			
+			when STATE_MOVE_CURSOR_NEXT =>
+			
+			when STATE_NEW_LINE =>
+			
+			when STATE_CFG =>
+			
+			when STATE_SET_CURSOR_POS =>
+		
+		end case;
+		
 	end process next_state;
 	
-	output : process(clk_cnt)
+	output : process(clk_cnt, lcd_state_next)
 	begin
+		lcd_state <= lcd_state_next;
+		
 		case lcd_state is
-			when STATE_INIT
+			when STATE_INIT =>
 				-- TODO: Here comes the initialization sequence for the LCD 
+			when STATE_IDLE =>
+			
+			when STATE_SET_CHAR =>
+			
+			when STATE_DELETE =>
+			
+			when STATE_CLR_SCREEN =>
+			
+			when STATE_MOVE_CURSOR_NEXT =>
+			
+			when STATE_NEW_LINE =>
+			
+			when STATE_CFG =>
+			
+			when STATE_SET_CURSOR_POS =>
+			
+			
 		end case;
 		-- TODO: set RS, RW, DB and EN depending on input
 	end process output;
@@ -46,7 +107,7 @@ begin
 	sync : process(clk_cnt) 
 	begin
 		if res_n = '0' then
-			receiver_state <= STATE_INIT;
+			lcd_state <= STATE_INIT;
 			
 			
 		elsif rising_edge(clk) then
--- src/lcd/src/lcd_pkg.vhdl
+++ src/lcd/src/lcd_pkg.vhdl
@@ -11,6 +11,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package lcd_controller_pkg is
+
+	function wait_lcd_is_busy(db: std_logic) return boolean;
+
+	constant INSTR_SET_CHAR 		: std_logic_vector(1 downto 0) := x"01";
+	constant INSTR_CLR_SCREEN 		: std_logic_vector(1 downto 0) := x"02";
+	constant INSTR_SET_CURSOR_POS : std_logic_vector(1 downto 0) := x"03";
+	constant INSTR_CONFIG			: std_logic_vector(1 downto 0) := x"04";
+	constant INSTR_DELETE 			: std_logic_vector(1 downto 0) := x"05";
+	constant INSTR_MOVE_CURSOR 	: std_logic_vector(1 downto 0) := x"06";
+	constant INSTR_NEW_LINE 		: std_logic_vector(1 downto 0) := x"07";
+
 	component lcd_controller is
 		generic (
 			CLK_FREQ	:	integer
@@ -30,5 +41,16 @@ package lcd_controller_pkg is
 		);
 	end component lcd_controller;
 end package lcd_controller_pkg;
+
+package body lcd_controller_pkg is 
+	function wait_lcd_is_busy(db: std_logic_vector(7 downto 0)) return boolean is 
+	begin
+			rs <= '0';
+			rw <= '1';
+			wait until db = '0';
+			rw <= '0';		
+			return false;
+	end function wait_lcd_is_busy;
+end lcd_controller_pkg;
 
 --- EOF ---
